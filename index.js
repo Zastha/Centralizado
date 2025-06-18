01f14ad6ead3e4684905fd95ab7e9b7311898747
GuillermoGonRata <L22170672@culiacan.tecnm.mx>
2025-06-18 12:19:41 -0700

    Puto el que lo lea uvu

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const input = '(add 2 (sub 4 3))';
 const output = compiler(input);
 console.log(JSON.stringify(output,null,2));
 console.log("manolo");
+console.log("Puto el que lo lea, manolo es pendejo");
